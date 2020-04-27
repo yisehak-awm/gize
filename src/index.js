691f02997e097279bbabf9e87bc55480a84f9bd8
Yisehak Abreham <yisehak.aw@gmail.com>
2020-04-27 04:00:24 -0700

    export functions

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { default as isEthiopicLeapYear } from "./isEthiopicLeapYear";
+export { default as isGregorianLeapYear } from "./isGregorianLeapYear";
+export { default as validateEthiopicDate } from "./validateEthiopicDate";
+export { default as validateGregorianDate } from "./validateGregorianDate";
+export { default as ethiopicToGregorian } from "./ethiopicToGregorian";
+export { default as gregorianToEthiopic } from "./gregorianToEthiopic";
